60219a4257e6aa7feb1bcb1cc7895bb99336ca04
Maks <m.o.kosianchuk@strudent.khai>
2026-02-03 23:01:56 +0200

    Added new loader, added loader animation on submit button

--- src/components/Ui/PrimaryButton/index.jsx
+++ src/components/Ui/PrimaryButton/index.jsx
@@ -1,9 +1,13 @@
 import "./PrimaryButton.scss";
+import Loader from "../Loading";
 
-export default function PrimaryButton({ children, onClick, type = "button", className = "" }) {
+export default function PrimaryButton({ children, onClick, type = "button", className = "", is_loading = false }) {
   return (
-    <button className={`primary_button app-transition ${className}`} onClick={onClick} type={type}>
-      {children}
+    <button className={`primary_button app-transition ${className} ${is_loading ? 'primary_button_loading' : ''}`} onClick={ is_loading ? () => {} : onClick} type={type}>
+      {is_loading && <Loader size={20}/>}
+      <p style={{ visibility: is_loading ? 'hidden' : 'visible' }}>
+        {children}
+      </p>
     </button>
   );
 }
